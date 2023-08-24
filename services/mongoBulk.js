1f53470be780f9dfa825941e2f759e80fbb090e1
Al Zanari <alzanari@protonmail.com>
2023-08-24 11:55:42 +0100

    initial bulk upsert

---
+++ services/mongoBulk.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const Lab = require("../models/lab");
+const Med = require("../models/med");
+
+async function labBulkUpsert(dataArray) {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const bulkOperations = dataArray.map((data) => {
+      const { link, ...updateData } = data;
+      return {
+        updateOne: {
+          filter: { link },
+          update: { $set: updateData },
+          upsert: true,
+        },
+      };
+    });
+
+    await Lab.bulkWrite(bulkOperations, { session });
+
+    await session.commitTransaction();
+  } catch (error) {
+    console.error("Error performing lab bulk :", error);
+    await session.abortTransaction();
+  } finally {
+    session.endSession();
+  }
+}
+
+async function medBulkUpsert(dataArray) {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const bulkOperations1 = dataArray.map((data) => {
+      const { link, similar, activeSubstance, ...updateData } = data;
+      return {
+        updateOne: {
+          filter: { link },
+          update: { $set: updateData },
+          upsert: true,
+        },
+      };
+    });
+
+    await Med.bulkWrite(bulkOperations1, { session });
+    let medsInDb = await Med.find({});
+
+    medsInDb.forEach((medDb) => {
+      medDb.similar.forEach((sim) => {
+        const foundObject = medsInDb.find((obj) => obj.link === sim.link);
+        sim._id = foundObject._id;
+      });
+      medDb.activeSubstance.forEach((act) => {
+        const foundObject = medsInDb.find((obj) => obj.link === act.link);
+        act._id = foundObject._id;
+      });
+    });
+
+    const bulkOperations2 = medsInDb.map((data) => {
+      const { _id, similar, activeSubstance } = data;
+      return {
+        updateOne: {
+          filter: { _id },
+          update: { $set: { similar, activeSubstance } },
+          upsert: true,
+        },
+      };
+    });
+
+    await Med.bulkWrite(bulkOperations2, { session });
+
+    await session.commitTransaction();
+  } catch (error) {
+    console.error("Error performing lab bulk :", error);
+    await session.abortTransaction();
+  } finally {
+    session.endSession();
+  }
+}
+
+module.exports = {
+  labBulkUpsert,
+  medBulkUpsert,
+};
