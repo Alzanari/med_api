783b4f6369c0e2cbfbbea4363d6e40454076c873
Al Zanari <alzanari@protonmail.com>
2023-08-24 17:10:41 +0100

    updated cronjob.js

--- services/cronjob.js
+++ services/cronjob.js
@@ -1,10 +1,12 @@
-const scrapper = require("./scapper");
 var cron = require("node-cron");
+const { getItems } = require("./scapper");
+const { labBulkUpsert } = require("./mongoBulk");
 
-// 0 0 3 * * *
+// TODO:
+// check the "Dernière mise à jour" in the footer and compare/store it in a json before running the scraper
 
 module.exports = () => {
-  cron.schedule("0 3 * * *", () => {
-    console.log("everyday at 3AM");
+  cron.schedule("0 3 * * *", async () => {
+    // console.log("everyday at 3AM");
   });
 };
