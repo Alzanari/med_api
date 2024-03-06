020c51513d4ae3a5cb7c407b3397c9ea6da0f771
AlZanari <alzanari@protonmail.com>
2024-03-06 18:01:39 +0100

    fixed jwt middleware

--- middlewares/jwt.middleware.js
+++ middlewares/jwt.middleware.js
@@ -1,22 +1,24 @@
 const jwt = require("jsonwebtoken");
 const winston = require("../config/winston.config");
 
-module.exports = function authenticateToken(req, res, next) {
-  const authHeader = req.headers["Authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+module.exports = function () {
+  return async function (req, res, next) {
+    const authHeader = req.headers["Authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    const tokenNotFoundError = new Error("unauthorized! token not provided");
-    winston.error(tokenNotFoundError.message);
-    return res.status(401).json({ error: tokenNotFoundError.message });
-  }
-  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-    if (err) {
-      const invalidTokenError = new Error("Invalid token");
-      winston.error(invalidTokenError.message);
-      return res.status(401).json({ error: invalidTokenError.message });
+    if (!token) {
+      const tokenNotFoundError = new Error("unauthorized! token not provided");
+      winston.error(tokenNotFoundError.message);
+      return res.status(401).json({ error: tokenNotFoundError.message });
     }
-    req.user = decoded;
-    next();
-  });
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if (err) {
+        const invalidTokenError = new Error("Invalid token");
+        winston.error(invalidTokenError.message);
+        return res.status(401).json({ error: invalidTokenError.message });
+      }
+      req.user = decoded;
+      next();
+    });
+  };
 };
